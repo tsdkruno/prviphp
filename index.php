0fa0ed86746bca04de1d390bfc7fb014ba30d44f
Krunoslav Husak <krunoslav.husak@skole.hr>
2016-03-09 13:34:45 +0100

    Dodali smo PHP kod i varijable.

--- index.php
+++ index.php
@@ -2,10 +2,24 @@
 <html>
 <head>
     <title>Prvi PHP</title>
+    <meta charset="utf-8">
 </head>
 
 <body>
 <h1>Prvi PHP</h1>
+
+<?php
+    echo('Ovo je prije odlomka<br>');
+    echo('<br>');
+    echo('Super, jos cemo dodati<br>');
+
+    echo(date('d.m.Y.'));
+
+    $ocjena = 5;
+    echo('<br>Vaša ocjena je: ' . $ocjena);
+    $nova_var = 166.8;
+    echo('<br>' . $nova_var);
+?>
 
 <p>
     Ovo je odlomak
